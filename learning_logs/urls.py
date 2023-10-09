33c1e943fb6dfec73544c051b2acb26db90c7f35
Alex_windows <perov97@yandex.ru>
2023-10-09 22:22:55 +0300

    Creating a Home Page

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""
+Определяет схемы URL для learning_logs
+"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    path('', views.index, name='index'),    # Домашняя страница
+]
+
